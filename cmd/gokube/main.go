4ca5cac8fa05fa0afcb80af25105c256992463f0
detvdl <detvdael.services@protonmail.com>
2020-06-17 22:48:14 +0200

    feat: simplified kubectx implementation

---
+++ cmd/gokube/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/detvdl/gokube/internal/kubernetes"
+	"github.com/detvdl/gokube/internal/platform/tty"
+	"github.com/manifoldco/promptui"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	path := "/home/vandaeld/.kube/config"
+	var c kubernetes.KubeConfig
+	err := c.FromFile(path)
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}:",
+		Active:   "\U000025b8 {{ .Name | cyan }}",
+		Inactive: "  {{if .IsCurrent}}{{.Name | green}}{{else}}{{.Name}}{{end}}",
+		Selected: promptui.IconGood + " {{ .Name | faint }}",
+	}
+	cs, err := c.GetContexts()
+	if err != nil {
+		log.Fatalf("Could not get contexts from kubeconfig: %v", err)
+	}
+	searcher := func(input string, index int) bool {
+		ctx := cs[index]
+		name := strings.Replace(strings.ToLower(ctx.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+	prompt := promptui.Select{
+		Label:             "Select Kubernetes context",
+		Items:             cs,
+		Templates:         templates,
+		Searcher:          searcher,
+		StartInSearchMode: true,
+		Size:              len(cs),
+		Stdout:            &tty.BellSkipper{},
+	}
+	i, _, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed: %q\n", err)
+		return
+	}
+	err = c.SetCurrentContext(cs[i].Name)
+	configString, err := yaml.Marshal(c)
+	if err != nil {
+		log.Fatalf("Failed to marshal edited yaml: %v\n", err)
+	}
+	ioutil.WriteFile(path, configString, os.ModePerm)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/detvdl/gokube
+
+go 1.14
+
+require (
+	github.com/manifoldco/promptui v0.7.0
+	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
+)
